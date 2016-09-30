a76e5d816a18e2ad26aa608f262baae5b243a246
Cieran Neeson <cieranneeson@gmail.com>
2016-09-30 17:21:41 +0100

    Testing a Modal

--- public/app/app.js
+++ public/app/app.js
@@ -1,1 +1,36 @@
 var app = angular.module('betgrade', ['ui.bootstrap']);
+
+
+app.controller('ModalCtrl', function ($scope, $uibModal, $log) {
+
+	  $scope.animationsEnabled = true;
+
+	  $scope.open = function (size) {
+
+	    var modalInstance = $uibModal.open({
+	      animation: $scope.animationsEnabled,
+	      templateUrl: 'form.html',
+	      controller: 'ModalInstanceCtrl',
+	      size: size,
+	   });
+
+	    modalInstance.result.then(function (selectedItem) {
+	      $scope.selected = selectedItem;
+	    }, function () {
+	      $log.info('Modal dismissed at: ' + new Date());
+	    });
+	  };
+
+	  $scope.toggleAnimation = function () {
+	    $scope.animationsEnabled = !$scope.animationsEnabled;
+	  };
+
+});
+
+app.controller('ModalInstanceCtrl', function ($scope, $uibModalInstance) {
+
+    $scope.cancel = function () {
+    $uibModalInstance.dismiss('cancel');
+    };
+
+});
