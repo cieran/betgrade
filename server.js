ab989768bbe8f29a64a636ee0b3371039cbb5e12
Cieran Neeson <cieranneeson@gmail.com>
2016-10-24 13:32:45 +0100

    ch-ch-ch-changes

--- server.js
+++ server.js
@@ -4,8 +4,24 @@
  * Module dependencies.
  */
 var express = require('express');
+var passport = require('passport');
+var bodyParser = require('bodyParser');
+var morgan = require('morgan');
+var mongoose = require('mongoose');
+var config = require('./config/database');
+var User = require('./app/models/user');
+var port = process.env.PORT || 3000;
 var app = express();
-// var app = require('./config/lib/app');
 
 app.use(express.static(__dirname + '/public'));
-app.listen(3000);
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
+
+app.use(morgan('dev'));
+app.use(passport.initialize());
+
+app.get('/', function(req, res){
+    res.send('hello, API at http://localhost:' + port + '/api');
+});
+app.listen(port);
+console.log('Listening at port ' + port);
