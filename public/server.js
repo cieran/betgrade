b84b686160daec4eaf394f126531f80c1df5df0a
Cieran Neeson <cieranneeson@gmail.com>
2016-10-25 14:24:49 +0100

    THINGS ARE WORKING

--- public/server.js
+++ public/server.js
@@ -48,6 +48,31 @@ apiRoutes.post('/signup', function(req, res){
        });
    }
 });
+
+apiRoutes.post('/authenticate', function(req, res) {
+  User.findOne({
+    username: req.body.username
+  }, function(err, user) {
+    if (err) throw err;
+ 
+    if (!user) {
+      res.send({success: false, msg: 'Authentication failed. User not found.'});
+    } else {
+      // check if password matches
+      user.comparePassword(req.body.password, function (err, isMatch) {
+        if (isMatch && !err) {
+          // if user is found and password is right create a token
+          var token = jwt.encode(user, config.secret);
+          // return the information including token as JSON
+          res.json({success: true, token: 'JWT ' + token});
+        } else {
+          res.send({success: false, msg: 'Authentication failed. Wrong password.'});
+        }
+      });
+    }
+  });
+});
+
 app.use('/api', apiRoutes);
 
 app.listen(port);
