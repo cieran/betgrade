115d4c04398c2704c8c612fa4de8895205519487
Cieran Neeson <cieranneeson@gmail.com>
2016-10-25 14:52:20 +0100

    maybe now

--- public/server.js
+++ public/server.js
@@ -14,7 +14,6 @@ var jwt = require('jwt-simple');
 var port = process.env.PORT || 3000;
 var app = express();
 
-app.use(express.static(__dirname + '/public'));
 
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
@@ -23,7 +22,7 @@ app.use(morgan('dev'));
 app.use(passport.initialize());
 
 app.get('/', function(req, res){
-    res.send('hello, API at http://betgrade.co:' + port + '/api');
+    res.redirect('/index.html');
 });
 mongoose.connect(config.database);
 
