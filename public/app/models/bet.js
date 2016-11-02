ff7ec3401cf96cb8f07ea83e597f345b62e651f7
Cieran Neeson <cieranneeson@gmail.com>
2016-11-02 14:23:23 +0000

    here we go

--- public/app/models/bet.js
+++ public/app/models/bet.js
@@ -1,27 +1,32 @@
+var express = require('express');
+var router = express.Router();
 var mongoose = require('mongoose');
+mongoose.connect('betgrade.co:27017/betgrade');
 var Schema = mongoose.Schema;
-var bcrypt = require('bcrypt');
+
 
 var BetSchema = new Schema({
-    username: {
-        type: String,
-        required: true
-    },
-    marketname: {
-        type: String,
-        required: true
-    },
-    option: {
-        type: String,
-        required: true
-    },
-    odds: {
-        type: Number,
-        required: true
-    },
-    amount: {
-        type: Number,
-        required: true
-    }
+    market: String,
+    bet: String,
+    amount: Number,
+    odds: Number,
+    user: String,
+    settled: Boolean
+}, {collection: 'bets'});
+
+var Bet = mongoose.model('Bet', BetSchema);
+
+router.get('/', function(req, res, next){
+   res.render('index');
 });
 
+router.get('/get-bets', function(req, res, next){
+    Bet.find()
+        .then(function(doc){
+        res.render('index', {items: doc});
+    });
+});
+
+
+
+module.exports = mongoose.model('Bet', BetSchema);
--- server.js
+++ server.js
@@ -19,9 +19,12 @@ var app = express();
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
-
+app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + '/views/layouts/'}));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
 app.use(morgan('dev'));
 app.use(passport.initialize());
+
 
 mongoose.connect(config.database);
 
