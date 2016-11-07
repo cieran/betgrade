52dfa3c2cfad7b68a6d3faed5168711901efd111
Cieran Neeson <cieranneeson@gmail.com>
2016-11-07 16:53:07 +0000

    GONNA BREAK EVERYTHING

--- public/models/user.js
+++ public/models/user.js
@@ -23,7 +23,7 @@ UserSchema.methods.generateHash = function(password){
 }
 
 UserSchema.methods.validPassword = function(password){
-    return bcrypt.compareSync(passport, this.password);
+    return bcrypt.compareSync(password, this.password);
 }
 
 module.exports = mongoose.model('User', UserSchema);
