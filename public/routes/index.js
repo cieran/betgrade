f38c2ee1081e7dedab4a8b06f6866eb9a5793272
Cieran Neeson <cieranneeson@gmail.com>
2016-11-07 16:23:27 +0000

    GONNA BREAK EVERYTHING

--- public/routes/index.js
+++ public/routes/index.js
@@ -1,4 +1,17 @@
-require('../models/bet.js');
+var MarketSchema = new Schema({
+    marketname: String,
+    markettype: String,
+    A: Number,
+    B: Number,
+    C: Number,
+    D: Number,
+    E: Number,
+    filename: String,
+    total: Number,
+    settled: Boolean
+}, {collection: 'markets'});
+
+var Market = mongoose.model('Market', MarketSchema);
 
 module.exports = function(app, passport){
     
