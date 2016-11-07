e952b59263a62d0b21759c1608141fc0c95f4536
Cieran Neeson <cieranneeson@gmail.com>
2016-11-07 16:01:47 +0000

    GONNA BREAK EVERYTHING

--- public/bin/server.js
+++ public/bin/server.js
@@ -17,8 +17,8 @@ mongoose.connect(db.database);
 // Creating View Engine which will render Handlebar files
 app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + '/views/layouts/'}));
 app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(path.join(__dirname, 'assets')));
 app.set('view engine', 'hbs');
+app.use(express.static(path.join(__dirname, 'assets')));
 
 app.use(morgan('dev'));
 app.use(bodyParser.json());
