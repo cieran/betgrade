e3a9a5214629b87e561a869822929eb9ddda22f7
Cieran Neeson <cieranneeson@gmail.com>
2016-11-07 20:34:38 +0000

    OH IT'S LIT

--- public/server.js
+++ public/server.js
@@ -50,19 +50,11 @@ app.use(function(req, res, next) {
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
   });
 }
 
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
 });
 
 
