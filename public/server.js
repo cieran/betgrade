804bf3ce6f153ba48aa79e2c4b88a21ba60e8347
Cieran Neeson <cieranneeson@gmail.com>
2016-11-07 16:04:10 +0000

    GONNA BREAK EVERYTHING

---
+++ public/server.js
@@ -0,0 +1,69 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash = require('connect-flash');
+var morgan = require('morgan');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var hbs = require('express-handlebars');
+var session = require('express-session');
+var db = require('../config/database.js');
+require('../config/passport')(passport);
+mongoose.connect(db.database);
+
+// Creating View Engine which will render Handlebar files
+app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + 'views/layouts/'}));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.use(express.static(path.join(__dirname, 'assets')));
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use(session({
+    secret: 'lionelrichie',
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+require('../routes/index.js')(app, passport);
+
+
+app.listen(port);
+console.log('Node Server Running @ Port: ' + port);
+
+// Boring error handling down here
+app.use(function(req, res, next) {
+  var err = new Error("where do you think you're going?");
+  err.status = 404;
+  next(err);
+});
+
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+
