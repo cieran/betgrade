ed7580398d213c11d1fdebff5da7cdff84f9b588
Cieran Neeson <cieranneeson@gmail.com>
2016-11-07 13:20:16 +0000

    GONNA BREAK EVERYTHING

--- public/routes/index.js
+++ public/routes/index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
-mongoose.connect('betgrade.co:27017/betgrade');
 var Schema = mongoose.Schema;
 
 /*
