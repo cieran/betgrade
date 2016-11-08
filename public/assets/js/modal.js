bc99a73df7b8f6e1b447b90a319beff4a85563d1
Cieran Neeson <cieranneeson@gmail.com>
2016-11-08 20:00:52 +0000

    last dash

--- public/assets/js/modal.js
+++ public/assets/js/modal.js
@@ -1,39 +1,28 @@
+$(document).ready(function () {
 
-            $(document).ready(function () {
+    autoRemoveAlert($('.alert'));
 
-                autoRemoveAlert($('.alert'));
+    function autoRemoveAlert(alertElement)
+    {
+        alertElement.fadeTo(2000, 800).slideUp(800, function () {
+            alertElement.slideUp(800);
+        });
+    }
+});
 
-                function autoRemoveAlert(alertElement)
-                {
-                    alertElement.fadeTo(2000, 800).slideUp(800, function () {
-                        alertElement.slideUp(800);
-                    });
-                }
-            });
-
-		window.onload = function(){ 
-			var modal = document.getElementById('myModal');
-
-			// Get the button that opens the modal
-			var btn = document.getElementById("myBtn");
-
-			// Get the <span> element that closes the modal
-			var span = document.getElementsByClassName("close")[0];
-
-			// When the user clicks the button, open the modal
-			btn.onclick = function() {
-				modal.style.display = "block";
-			}
-
-			// When the user clicks on <span> (x), close the modal
-			span.onclick = function() {
-				modal.style.display = "none";
-			}
-
-			// When the user clicks anywhere outside of the modal, close it
-			window.onclick = function(event) {
-				if (event.target == modal) {
-					modal.style.display = "none";
-				}
-			}
-		};
+window.onload = function(){ 
+    var modal = document.getElementById('myModal');
+    var btn = document.getElementById("myBtn");
+    var span = document.getElementsByClassName("close")[0];
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+};
