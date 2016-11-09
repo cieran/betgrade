d3172b76c83c380b8cd1bed569842a97011fe6ba
Cieran Neeson <cieranneeson@gmail.com>
2016-11-09 13:31:42 +0000

    so done

--- public/routes/markets.js
+++ public/routes/markets.js
@@ -1,21 +1,6 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var MarketSchema = new Schema({
-    marketname: String,    
-    student: String,
-    A: Number,
-    B: Number,
-    C: Number,
-    D: Number,
-    E: Number,
-    filename: String,
-    total: Number,
-    settled: Boolean
-}, {collection: 'markets'});
-
-var Market = mongoose.model('Market', MarketSchema);
-
 module.exports = function(app, passport){
     app.get('/markets/:filename', function(req, res, next){
         var filename = req.params.filename;
