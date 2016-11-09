eb820ad2935e0fb58487ece700de465ac12002c5
Cieran Neeson <cieranneeson@gmail.com>
2016-11-09 13:34:23 +0000

    so done

--- public/routes/markets.js
+++ public/routes/markets.js
@@ -1,13 +1,13 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-    app.get('/markets/:filename', function(req, res, next){
+app.get('/markets/:filename', function(req, res, next){
         var filename = req.params.filename;
         Market.find({"filename" : filename})
             .then(function(doc){
                 res.render(filename, {title: 'Markets', markets: doc, user: req.user});
         });
-    });
+ });
     
     
 module.exports = router;
