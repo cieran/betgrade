fa4d969889c33f00874ce98ed35650b34202fcdd
Cieran Neeson <cieranneeson@gmail.com>
2016-11-17 13:22:22 +0000

    updates

--- public/routes/index.js
+++ public/routes/index.js
@@ -79,7 +79,7 @@ module.exports = function(app, passport){
         var removal_code = req.body.code;
         Market.find({"student" : student_name, "code" : removal_code}, function(err, user){
             if(user){
-            Market.remove({"student" : student_name}){
+            Market.remove({"student" : student_name}, function(err){
                 if(err){
                     console.log(err);
                     res.redirect('/optout');
@@ -87,7 +87,7 @@ module.exports = function(app, passport){
                     console.log("user removed");
                     res.redirect('/');
                 } 
-            }
+            });
             }
         });
     });
