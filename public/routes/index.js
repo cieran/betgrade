c2a54492a79a1a64deaa41ee13ab4e08df5017da
Cieran Neeson <cieranneeson@gmail.com>
2016-11-17 13:28:19 +0000

    updates

--- public/routes/index.js
+++ public/routes/index.js
@@ -79,7 +79,7 @@ module.exports = function(app, passport){
         var removal_code = req.body.code;
         Market.find({"student" : student_name, "code" : removal_code}, function(err, user){
             if(user){
-            Market.remove({"student" : student_name, "code" : removal_code}, function(err){
+            Market.remove({"student" : student_name, "code" : removal_code}, function(){
                 if(err){
                     console.log(err);
                     res.redirect('/optout');
