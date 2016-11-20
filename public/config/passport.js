735d426224b04a6c4b99d1b52d3ed2f3eef2a415
Cieran Neeson <cieranneeson@gmail.com>
2016-11-20 23:17:56 +0000

    used : true error

--- public/config/passport.js
+++ public/config/passport.js
@@ -46,13 +46,14 @@ module.exports = function(passport){
                         }else{
                             saved = true;
                             console.log("Updating " + snumber);
-                            return done(null, newUser);
+                            
                         }
                     });
                 }
         }); 
         if(saved == true){
             StudentNumber.update({'number' : snumber}, {$set : {'used': true}});
+            return done(null, newUser);
         }     
         });
             
