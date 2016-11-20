7e05746771b8432bb62186065e1a61027d9c804f
Cieran Neeson <cieranneeson@gmail.com>
2016-11-20 21:57:08 +0000

    used : true error

--- public/config/passport.js
+++ public/config/passport.js
@@ -32,6 +32,7 @@ module.exports = function(passport){
             }else{
                User.findOne({'username':username}, function(err, user){
                 if(err)
+                    console.log(err);
                     return done(err);
                 if(user){
                     return done(null, false, req.flash('signupMessage', 'Username already registered.'));
