4163aa1660edc7193ff3c54c151d3d47322ebd0a
Cieran Neeson <cieranneeson@gmail.com>
2016-11-21 22:58:48 +0000

    used : true error

--- public/assets/js/modal.js
+++ public/assets/js/modal.js
@@ -1,7 +1,5 @@
 $(document).ready(function () {
-
     autoRemoveAlert($('.alert'));
-
     function autoRemoveAlert(alertElement)
     {
         alertElement.fadeTo(2000, 800).slideUp(800, function () {
@@ -9,21 +7,24 @@ $(document).ready(function () {
         });
     }
 });
-
-    var modal = document.getElementById('myModal');
-    var span = document.getElementsByClassName("close");
-    span.onclick = function(event) {
-        if(event.target == modal){
-            modal.style.display = "none";
-        }
+$(document).on("click", "#myModal", function(){
+    var marketname = $(this).data('id');
+    $(".modal-content #marketname").val(marketname);
+});
+var modal = document.getElementById('myModal');
+var span = document.getElementsByClassName("close");
+span.onclick = function(event) {
+    if(event.target == modal){
+        modal.style.display = "none";
     }
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
+}
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
     }
-    window.onkeydown = function(e){
-        if(e.keyCode == 27){
-            modal.style.display = "none";
-        }
+}
+window.onkeydown = function(e){
+    if(e.keyCode == 27){
+        modal.style.display = "none";
     }
+}
