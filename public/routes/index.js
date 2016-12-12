13d78c1b095604a1630811998ea62d4db26de423
Cieran Neeson <cieranneeson@gmail.com>
2016-12-12 12:50:56 +0000

    Catagories Page

--- public/routes/index.js
+++ public/routes/index.js
@@ -43,7 +43,6 @@ module.exports = function(app, passport){
     app.get('/signup', function(req, res){
         res.render('signup', { message: req.flash('signupMessage') });
     });
-
     app.post('/signup', passport.authenticate('local-signup',{
         successRedirect : '/profile',
         failureRedirect : '/signup',
