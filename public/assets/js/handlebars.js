4ddccd5774b3a67dfbbef8709d685a59ae6667ac
Cieran Neeson <cieranneeson@gmail.com>
2016-12-13 18:56:14 +0000

    Leaderboard

--- public/assets/js/handlebars.js
+++ public/assets/js/handlebars.js
@@ -1,3 +1,15 @@
-Handlebars.registerHelper("inc", function(value){
-    return parseInt(value) + 1;
-});
+function hbsHelpers(hbs) {
+  return hbs.create({
+    helpers: { // This was missing
+      inc: function(value, options) {
+        console.log('reading it');
+        return parseInt(value) + 1;
+      }
+
+      // More helpers...
+    }
+
+  });
+}
+
+module.exports = hbsHelpers;
