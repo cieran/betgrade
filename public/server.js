56a3a202cac3b26fae99b00d80c1c3494a96ffb7
Cieran Neeson <cieranneeson@gmail.com>
2016-12-14 11:53:41 +0000

    Profiles

--- public/server.js
+++ public/server.js
@@ -20,7 +20,7 @@ mongoose.connect(db.database);
 var hbs = exphbs.create({
     helpers: {
         inc : function(value) { return parseInt(value) + 1;},
-        date: function(date){  return moment(date).format('YYYY-MM-DD hh:mm:ss');}
+        date: function(date){  return moment(date).format('YY-MM-DD hh:mm');}
     },
     extname: 'hbs', 
     defaultLayout: 'layout', 
