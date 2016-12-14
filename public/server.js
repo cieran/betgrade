7b85de30776f73388f55d00f698aa6b0fd4add3e
Cieran Neeson <cieranneeson@gmail.com>
2016-12-14 13:59:25 +0000

    Profiles

--- public/server.js
+++ public/server.js
@@ -23,7 +23,6 @@ var hbs = exphbs.create({
         inc : function(value) { return parseInt(value) + 1;},
         date: function(date){  return moment(date).tz('Europe/Dublin').format('DD-MM-YYYY hh:mm');},
         ifeq: function(a, b, options){
-            console.log(a + "" + b);
             if(a === b)
                 return options.fn(this);
         }
