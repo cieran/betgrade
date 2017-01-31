ca62ca341967eb8daa9571a64dcafe095ea457ea
Cieran Neeson <cieranneeson@gmail.com>
2017-01-31 00:06:28 +0000

    Pairing Bets

--- public/routes/update.js
+++ public/routes/update.js
@@ -23,7 +23,7 @@ var Bet = require('../models/bet');
 var stake = 0;
 setInterval(function() {
 	stake = Bet.find({"paired" : false}, {_id:0, bet:1, market:1, student:1, stake:1}).sort({createdAt : 1}).limit(1)
-		.then(function(doc){
+		.map(function(doc){
 			var temp = doc[0].stake;
 			console.log("Temp: " + temp);
 			return temp;
