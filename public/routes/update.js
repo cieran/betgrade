7b9ebaee0703f9b1ef361421862178f20c111f47
Cieran Neeson <cieranneeson@gmail.com>
2017-02-03 20:02:52 +0000

    Async Translation

--- public/routes/update.js
+++ public/routes/update.js
@@ -28,15 +28,16 @@ module.exports = {
 				var opp_settled = opp_doc.settled;
 				if(temp_to_match <= opp_to_match){
 					temp_to_match -= opp_to_match;
-					opp_to_match -= temp_to_match;
 					if(temp_to_match <= 0){
-						temp_to_match = 0;
 						paired = true;
+						opp_to_match += temp_to_match;
+						temp_to_match = 0;
 
 					}
 					if(opp_to_match <= 0){
-						opp_to_match = 0;
 						opp_paired = true;
+						opp_to_match -= temp_to_match;
+						opp_to_match = 0;
 					}
 					Bet.update({"_id" : id}, 
 						{$set : {'to_match': temp_to_match,'paired' : paired}}
@@ -60,54 +61,7 @@ module.exports = {
 		}, function(err){
 			console.log("done");
 		})});
-	},
-	sync_match : function(){
-		Bet.find({"paired" : false}, {_id:1, bet:1, market:1, odds:1, student:1, to_match:1, stake:1})
-		.sort({createdAt : 1})
-			.then(function(doc){
-				for(var j = 0; j < doc.length; j++){
-				var result = doc[j];
-				var id = result._id;
-				var stake = result.stake;
-				var odds = result.odds;
-				var market = result.market;
-				var student = result.student;
-				var side = result.bet;
-				var to_match = result.to_match;
-				Bet.find({"student":student, "market":market, "paired":false, "settled":false, 
-					"bet": {$ne : side}, "_id" : {$ne : id}}).then(function(results){
-					for(var i = 0; i < 2; i++){
-						var temp_to_match = to_match;
-						var array = results[i];
-						var opp_id = array._id;
-						console.log("Comparing " + id + " with " + opp_id);
-						var opp_paired = array.paired;
-						var opp_to_match = array.to_match;
-						var opp_settled = array.settled;
-						if(temp_to_match <= opp_to_match){
-							temp_to_match -= opp_to_match;
-							opp_to_match -= temp_to_match;
-							if(temp_to_match <= 0){
-								paired = true;
-
-							}
-							if(opp_to_match <= 0){
-								opp_paired = true;
-							}
-						Bet.findOneAndUpdate({"_id" : id}, {$set : {'to_match': temp_to_match,'paired' : paired}}, {new : true});
-						Bet.findOneAndUpdate({"_id" : opp_id}, {$set : {'to_match': opp_to_match, 'paired' : opp_paired}}, {new : true});
-						}
-					}
-
-				});
-
-		}});
-	},
-	settle : function(){
-			return null;
 	}
-
-
 };
 
 /*	
