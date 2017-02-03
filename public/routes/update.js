0d33937654a356c1203dc8a2f3a5a80a050c47c9
Cieran Neeson <cieranneeson@gmail.com>
2017-02-03 18:14:25 +0000

    Async Translation

--- public/routes/update.js
+++ public/routes/update.js
@@ -7,15 +7,38 @@ var Bet = require('../models/bet');
 
 module.exports = {
 	match : function(){
-		async.forEach(
-			Bet.find({"paired" : false}, {_id:1, bet:1, market:1, odds:1, student:1, to_match:1, stake:1})
-			.sort({createdAt : 1}), 
-		function(doc, callback){
-			console.log(doc[1]);
-			var opp_results = doc.length;
+		var results = Bet.find({"paired" : false}, {_id:1, bet:1, market:1, odds:1, student:1, to_match:1, stake:1})
+		.sort({createdAt : 1});
+		async.forEach(results, function(doc, callback){
+			var result = doc[0];
+			var id = result._id;
+			var stake = result.stake;
+			var odds = result.odds;
+			var market = result.market;
+			var student = result.student;
+			var side = result.bet;
+			var to_match = result.to_match;
+			var opp_results = Bet.find({"student":student, "market":market, "paired":false, "settled":false, 
+				"bet": {$ne : side}, "_id" : {$ne : id}});
 			async.forEach(opp_results, function(opp_doc, callback2){
-				console.log("we in here");
-				console.log(opp_results);
+				var temp_to_match = to_match;
+				var array = opp_doc[0];
+				var opp_id = array._id;
+				console.log("Comparing " + id + " with " + opp_id);
+				var opp_paired = array.paired;
+				var opp_to_match = array.to_match;
+				var opp_settled = array.settled;
+				if(temp_to_match <= opp_to_match){
+					temp_to_match -= opp_to_match;
+					opp_to_match -= temp_to_match;
+					if(temp_to_match <= 0){
+						paired = true;
+
+					}
+					if(opp_to_match <= 0){
+						opp_paired = true;
+					}
+				}
 			}, callback);
 		}, function(err){
 			console.log("done");
