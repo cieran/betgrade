a49a9f2c428bb378f8dc83a140d24058374ae8ce
Cieran Neeson <cieranneeson@gmail.com>
2017-02-04 22:23:41 +0000

    Dynamic Odds

--- public/routes/update.js
+++ public/routes/update.js
@@ -17,7 +17,7 @@ module.exports = {
 			var student = doc.student;
 			var side = doc.bet;
 			var to_match = doc.to_match;
-			Bet.find({"student":student, "odds":odds, "market":market, "paired":false, "settled":false, 
+			Bet.findOne({"student":student, "odds":odds, "market":market, "paired":false, "settled":false, 
 			"bet": {$ne : side}, "_id" : {$ne : id}}).sort({createdAt: 1}).exec(function(errs, res){
 			async.forEach(res, function(opp_doc, callback2){
 				var temp_to_match = to_match;
