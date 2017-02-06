7c2245d412a335074d68491346350d1b2a531852
Cieran Neeson <cieranneeson@gmail.com>
2017-02-06 23:20:50 +0000

    fancy shmancy bet table

--- public/routes/index.js
+++ public/routes/index.js
@@ -17,7 +17,7 @@ module.exports = function(app, passport){
     app.get('/profile/bet-history', function(req, res, next){
         var user = req.user;
         if(user){
-            Bet.find({$query : {"username" : user.username}, $orderby : {_id : -1}})
+            Bet.find({$query : {"username" : user.username, "paired" : true}, $orderby : {_id : -1}})
                 .then(function(doc){
                     res.render('profile/bet-history', {title: "Bet History | Betgrade", bets: doc, user: req.user}); 
             });
