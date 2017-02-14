f21fe21070d21215684a1e1341592262e41c94a0
Cieran Neeson <cieranneeson@gmail.com>
2017-02-14 22:04:04 +0000

    pls

--- public/routes/update.js
+++ public/routes/update.js
@@ -116,45 +116,41 @@ var object = {
 	findValue: function(x){
 		Market.find({"student" : x.student, "marketname": x.market}).sort({btotal: -1}).limit(1)
 			.then(function(doc){
-				doc.forEach(function(res){
-				console.log(student + ", " + market + ", " + res.btotal);
+				var res = doc[0];
+				console.log(x.student + ", " + x.market + ", " + res.btotal);
 				x.mostPopularOdds = res.back;
 				x.mostPopularBtotal = res.btotal;
 				object.findValueBelow(x);
-				})
 			});
 
 	},
 	findValueBelow: function(x){
      	Market.find({"student" : x.student, "marketname": x.market, "back" : {$lte : x.mostPopularBtotal}}).sort({odds: -1}).limit(1)
         	.then(function(doc){
-        		doc.forEach(function(res){
+        			var res = doc[0];
 	             x.valueBelowOdds = res.back;
 	             x.valueBelowBtotal = res.btotal;
 	             object.findValueAbove(x);
 	             console.log("   Below Odds: " + res.back + ", Below Backed: " + res.btotal);
-	         	})
 	         });
 	},
 	findValueAbove: function(x){
 	    Market.find({"student" : student, "marketname": market, "back" : {$gte : x.mostPopularBtotal}}).sort({odds: 1}).limit(1)
 	         .then(function(doc){
-	         	doc.forEach(function(res){
+	         		var res = doc[0];
 	             x.valueAboveOdds = res.lay;
 	             x.valueAboveLtotal = res.ltotal;
 	             object.findValueAboveAbove(x);
 				 console.log("   Above Odds: " + res.lay + ", Above Lay: " + res.ltotal);
-				})
 	         });
 	},
 	findValueAboveAbove : function(x){
 	    Market.find({"student" : x.student, "marketname": x.market, "back" : {$gte : x.mostPopularBtotal}}).sort({odds: 1}).skip(1).limit(1)
 	         .then(function(doc){
-	         	doc.forEach(function(res){
+	         		var res = doc[0];
 	         	 x.valueAboveAboveOdds = res.lay;
 	             x.valueAboveAboveLtotal = res.ltotal;
 	             console.log("   Second Above Odds: " + res.lay + ", Second Above Lay: " + res.ltotal);
-	         	})
 	         });
 	             
 	}
