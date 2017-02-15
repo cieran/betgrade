e5bfeb775ca13ae500a20619904e627cc0551ccb
Cieran Neeson <cieranneeson@gmail.com>
2017-02-15 13:18:20 +0000

    here we go again

--- public/routes/update.js
+++ public/routes/update.js
@@ -104,7 +104,7 @@ var object = {
 				var profit = returns - x.stake;
 				var diff = profit - liability;
 				var cashout_long = x.stake + (diff / doc[0].back);
-				var cashout = Math.round(cashout_long * 10 ) / 10;
+				var cashout = Math.round(cashout_long * 100 ) / 100;
 				x.cashout = cashout;
 				x.returns = cashout - x.stake;
 			});
@@ -139,7 +139,30 @@ var object = {
 					            x.valueBelowBtotal = res1.btotal;
 					        }
 					        console.log(x.student + " 2 popular back:  " + x.valueBelowOdds);
-					        
+					        Market.find({"student" : x.student, "marketname": x.marketname, "lay" : {$gt : x.mostPopularOdds}}).sort({odds: 1}).limit(1)
+				         	.then(function(doc2){
+				         	var res2 = doc2[0];
+				         	if(res2 == null){
+				         		x.valueAboveOdds = 0;
+				             	x.valueAboveLtotal = 0;
+				         	}else{
+				         		x.valueAboveOdds = res2.lay;
+				             	x.valueAboveLtotal = res2.ltotal;
+				         	}
+							console.log(x.student + " 1 popular lay:  " + x.valueAboveOdds);
+							Market.find({"student" : x.student, "marketname": x.market, "back" : {$gt : x.mostPopularOdds}}).sort({odds: 1}).skip(1).limit(1)
+					         .then(function(doc3){
+					         	var res3 = doc3[0];
+					         	if(res3 == null){
+					         		x.valueAboveAboveOdds = 0;
+					             	x.valueAboveAboveLtotal = 0;
+					         	}else{
+					         		x.valueAboveAboveOdds = res3.lay;
+					             	x.valueAboveAboveLtotal = res3.ltotal;
+					         	}
+								console.log(x.student + " 2 popular lay:  " + x.valueAboveAboveOdds);
+					         });
+				         });
 				});
 		});
 	},
