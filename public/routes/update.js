7a919d37867740bdf6a67a4e0f07021d41442c04
Cieran Neeson <cieranneeson@gmail.com>
2017-02-15 13:28:10 +0000

    here we go again

--- public/routes/update.js
+++ public/routes/update.js
@@ -98,7 +98,7 @@ var object = {
 				var cashout_long = x.stake + (diff / doc[0].back);
 				var cashout = Math.round(cashout_long * 100) / 100;
 				console.log(cashout);
-				var return_val = Math.round(cashout - x.stake * 100)/100;
+				var return_val = Math.round((cashout - x.stake) * 100)/100;
 				console.log(return_val);
 				x.cashout = cashout;
 				x.returns = return_val;
