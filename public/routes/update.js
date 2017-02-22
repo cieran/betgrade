dcf2e0f1b33530ddb9cf77931724022036275cd0
Cieran Neeson <cieranneeson@gmail.com>
2017-02-22 14:10:27 +0000

    callback

--- public/routes/update.js
+++ public/routes/update.js
@@ -201,7 +201,7 @@ var object = {
 				            x.valueBelowOdds = res.back;
 				            x.valueBelowBtotal = res.btotal;
 				        }
-					console.log(x.student + " newFindValueBelow");
+				        callback();
 			        });
 				}, function(err){
 					if(err)
