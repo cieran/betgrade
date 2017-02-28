d802ceaac3ce8df56fbde1d59e32ecce7bbff2c1
Cieran Neeson <cieranneeson@gmail.com>
2017-02-28 14:52:10 +0000

    cashout fix

--- public/routes/update.js
+++ public/routes/update.js
@@ -87,8 +87,9 @@ var object = {
 		var ret = parseFloat(object.static_calcReturns(x.bet, x.stake, x.odds));
 		console.log("ret from static func: " +ret);
 			Market.find({"marketname" : x.market, "student" : x.student, "ltotal": {$gte : {ret}}}).sort({ltotal:-1}).limit(1)
-			.then(function(doc){
-				console.log("doc " + doc[0]);
+			.then(function(errs, doc){
+				if(errs)
+					return console.log(errs);
 				if(!doc.length){
 					x.cashout = x.stake - 0.1;
 					x.returns = -0.1;
