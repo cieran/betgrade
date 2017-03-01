56d593655bf7a8ed15404f156efddc374ec6f909
Cieran Neeson <cieranneeson@gmail.com>
2017-03-01 13:53:18 +0000

    nav styling

--- public/assets/js/modal.js
+++ public/assets/js/modal.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
     }
 
     $('.close').click(function(){
-        $('.myModal, .modal-content').hide();
+        $('.myModal, .modal-content').modal('hide');
     });
     $('.modal-content').click(function(e){
         e.stopPropagation();
@@ -29,7 +29,7 @@ $(document).on("click", ".addmarket", function(){
     $(".modal-content #side").val(side);
 });
 
-var modal = document.getElementByClassName("myModal");
+var modal = document.getElementById('myModal');
 var span = document.getElementsByClassName("close");
 span.onclick = function(event) {
     if(event.target == modal){
