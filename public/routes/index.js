5db0089e09ddb770f1fc6492e37f1bc222c2c434
Cieran Neeson <cieranneeson@gmail.com>
2017-03-07 10:37:04 +0000

    bruteforce maybe

--- public/routes/index.js
+++ public/routes/index.js
@@ -21,8 +21,8 @@ var failCallback = function (req, res, next, nextValidRequestDate) {
 var stopThem = new ExpressBrute(store, {
     freeRetries:2, 
     refreshTimeoutOnRequest: false,
-    minWait: 1000 * 60 * 5,
-    maxWait: 1000 * 60 * 15,
+    minWait: 1000 * 60 * 1,
+    maxWait: 1000 * 60 * 5,
     failCallback: failCallback,
 });
 
