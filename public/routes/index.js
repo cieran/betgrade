53886921aa9bcedd3fc58747ca7c410c950c59bb
Cieran Neeson <cieranneeson@gmail.com>
2017-03-07 09:46:14 +0000

    bruteforce maybe

--- public/routes/index.js
+++ public/routes/index.js
@@ -6,6 +6,18 @@ var User = require('../models/user');
 var Bet = require('../models/bet');
 var Participant = require('../models/participant');
 var async = require('async');
+var bouncer = require ("express-bouncer")(500, 900000);
+
+// Add white-listed addresses (optional)
+bouncer.whitelist.push ("127.0.0.1");
+
+// In case we want to supply our own error (optional)
+bouncer.blocked = function (req, res, next, remaining)
+{
+    res.send (429, "Too many requests have been made, " +
+        "please wait " + remaining / 1000 + " seconds");
+};
+/**
 var ExpressBrute = require('express-brute');
 var MemcachedStore = require('express-brute-memcached');
 var moment = require('moment');
@@ -21,8 +33,7 @@ var stopThem = new ExpressBrute(store, {
     maxWait: 1000 * 60 * 15,
     failCallback: failCallback,
 });
-
-
+**/
 module.exports = function(app, passport){
     app.get('/', function(req, res, next){
         Market.find({"marketname" : 'To Pass'}).sort({btotal : -1})
@@ -319,7 +330,7 @@ module.exports = function(app, passport){
         }
     });
 
-    app.post('/login', stopThem.prevent, passport.authenticate('local-login', {
+    app.post('/login', bouncer.block, passport.authenticate('local-login', {
         successRedirect : '/profile',
         failureRedirect : '/login',
         failureFlash : true
