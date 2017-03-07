f13a3eed373b47e8f0fd8362e8fe4ee15258bc69
Cieran Neeson <cieranneeson@gmail.com>
2017-03-07 10:09:54 +0000

    bruteforce maybe

--- public/routes/index.js
+++ public/routes/index.js
@@ -14,8 +14,8 @@ bouncer.whitelist.push ("127.0.0.1");
 // In case we want to supply our own error (optional)
 bouncer.blocked = function (req, res, next, remaining)
 {
-    res.send (429, "Too many requests have been made, " +
-        "please wait " + remaining / 1000 + " seconds");
+    req.flash('error', "No bruteforcing please. You can come off the naughty step "+moment(remaining).fromNow()+ ".");
+    res.redirect('/');
 };
 /**
 var ExpressBrute = require('express-brute');
